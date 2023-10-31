8c935419a777fef1e74fba135a0f301e9a899e06
Ju <50602425+jumalley@users.noreply.github.com>
2023-10-31 22:17:31 +0100

    Create webhooks.lua

---
+++ webhooks.lua
@@ -0,0 +1,23 @@
+--- @param Important, add a webhook here, otherwise the images won't work.
+
+--- @param Set your own discord Webhook here.
+Config.Webhook = 'https://discord.com/api/webhooks/1169019783900049501/PfU6uuON-j9ryzDxSmHn4CkgT9V7zpkbKMa83RUECwJujMMwXwDa_NEE2o5yHfKu9LZD'
+
+--- @param Set your own discord Webhook here.
+Config.TwitterWebhook = 'https://discord.com/api/webhooks/1169022206370648185/mFe1JRJnNiVadqDBoD6A8Cw2noFO5JTI9CRXufXEyeELvY6ZSWEahhzDw-m15v_X9Xk9'
+
+--- @param Set your own discord Webhook here.
+Config.InstagramWebhook = 'https://discord.com/api/webhooks/1169022146488586290/ScZDmOloo-8Cs8yEeN7Pp2IUp582QfcFWw1mxDEDYYlWfOTZl0drxtRTACKMhZw9-CJO'
+
+--- @param Set your own discord Webhook here.
+Config.YellowPagesWebhook = ''
+
+--- @param Set your own discord Webhook here.
+Config.NewsWebhook = ''
+
+--- @param Set your own discord Webhook here.
+Config.DarkWebBuy = ''
+
+--- @param Set your own discord Webhook here.
+-- You can modify on qs-smartphone/server/custom/misc/bank.lua
+Config.BankWebhook = ''
